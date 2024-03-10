4f6e0d39608b0eb2c57fbfb6c4029a7ee49b29df
junaaid96 <info.junaidulislam@gmail.com>
2024-03-10 12:43:40 +0600

    minor formatting

--- patient/views.py
+++ patient/views.py
@@ -32,8 +32,7 @@ class PatientRegistrationView(FormView):
         uid = urlsafe_base64_encode(force_bytes(user.pk))
         print("uid: ", uid)
 
-        confirmation_url = f"https://health-ops-connect-mvt.onrender.com/patients/activate/{
-            uid}/{token}/"
+        confirmation_url = f"https://health-ops-connect-mvt.onrender.com/patients/activate/{uid}/{token}/"
 
         mail_subject = "Activate Your Account"
         mail_body = render_to_string('email/activation_email.html', {
